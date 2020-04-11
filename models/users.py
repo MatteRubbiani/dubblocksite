b433c9a6c4ce439434719af55e94dde51ca968d6
Matteo <matte.rubbiani@gmail.com>
2020-04-11 19:38:10 +0200

    h

--- models/users.py
+++ models/users.py
@@ -30,7 +30,7 @@ class UserModel(db.Model):
         self.jolly_reveal = 0
 
     def give_random_corsia(self, corsie):
-        self.corsia = intrandom.randint(0, int(corsie))
+        self.corsia = random.randint(0, int(corsie))
         self.save_to_db()
 
     @classmethod
